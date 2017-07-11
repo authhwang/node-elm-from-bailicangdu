2be7ec1ae68b796177f54167dea7c49faba05377
郭漫丽 <404544332@qq.com>
2017-07-11 11:24:38 +0800

    完成获取餐馆详情api

--- routes/shopping.js
+++ routes/shopping.js
@@ -4,7 +4,9 @@ const categoryHandler = require('../controller/shopping/category.js');
 
 
 router.get('/restaurants',shopHandler.getRestaurants);
-router.get('/v2/restaurants/category',categoryHandler.getCategories);
+router.get('/restaurant/:restaurant_id',shopHandler.getRestaurantDatail);
 router.get('/v1/restaurants/delivery_modes',categoryHandler.getDelivery);
 router.get('/v1/restaurants/activity_attributes',categoryHandler.getActivity);
+router.get('/v2/restaurants/category',categoryHandler.getCategories);
+
 module.exports = router;
